4ade150428564dedd4870d1131543b55cc896d71
celine5 <42863073+celine5@users.noreply.github.com>
2019-02-08 12:07:23 +0530

    Create prime 13.py

---
+++ prime 13.py
@@ -0,0 +1,14 @@
+num = 11
+  
+if num > 1: 
+      
+   for i in range(2, num//2): 
+     
+       if (num % i) == 0: 
+           print("no") 
+           break
+   else: 
+       print("yes") 
+  
+else: 
+   print("no")
