dc308cde5e7fcae6c02bc71d489c583cc96521ee
celine5 <42863073+celine5@users.noreply.github.com>
2019-02-18 14:54:40 +0530

    Create mysql3.py

---
+++ mysql3.py
@@ -0,0 +1,16 @@
+CREATE TABLE myfriends
+(
+    last_name   VARCHAR(15) NOT NULL,
+    first_name  VARCHAR(15) NOT NULL,
+    suffix      VARCHAR(5) NULL,
+    sex         VARCHAR(1) NULL,
+    city        VARCHAR(20) NOT NULL,
+    state       VARCHAR(2) NOT NULL,
+    age     int
+);
+
+ALTER TABLE myfriends ADD ROLLNUMBER SMALLINT UNSIGNED;
+
+ALTER TABLE myfriends ADD INDEX(ROLLNUMBER);
+
+DESCRIBE myfriends;
