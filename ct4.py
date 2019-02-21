56817434e1bb845320df2c4105e6b39f62efdb62
celine5 <42863073+celine5@users.noreply.github.com>
2019-02-21 14:10:23 +0530

    Create ct4.py

---
+++ ct4.py
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+bool uniqueCharacters(string str) 
+{ 
+  
+    // If at any time we encounter 2 
+    // same characters, return false 
+    for (int i = 0; i < str.length(); i++) { 
+        for (int j = i + 1; j < str.length(); j++) { 
+            if (str[i] == str[j]) { 
+                return false; 
+            } 
+        } 
+    } 
+  
+    // If no duplicate characters encountered, 
+    // return true 
+    return true; 
+} 
+  
+// driver code 
+int main() 
+{ 
+    string str = "GeeksforGeeks"; 
+  
+    if (uniqueCharacters(str)) { 
+        cout << "The String " << str 
+             << " has all unique characters\n"; 
+    } 
+    else { 
+        cout << "The String " << str 
+             << " has duplicate characters\n"; 
+    } 
+    return 0; 
+} 
